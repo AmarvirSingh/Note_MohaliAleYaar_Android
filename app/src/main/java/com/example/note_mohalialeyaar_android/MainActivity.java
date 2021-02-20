00fd285e581e13f13d4cfa50a0b82a4d508a5553
Amarvir Singh <amarvirgill06@gmail.com>
2021-02-20 16:00:26 -0500

    MainActivity toolBAr button added

--- app/src/main/java/com/example/note_mohalialeyaar_android/MainActivity.java
+++ app/src/main/java/com/example/note_mohalialeyaar_android/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.note_mohalialeyaar_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+       getMenuInflater().inflate(R.menu.mainactivity_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case(R.id.deleteBtn):
+                Toast.makeText(this,"Delete button clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case(R.id.folderBtn):
+                Toast.makeText(this, "Show Folder Butoon Clicked ", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.search):
+                Toast.makeText(this, "Search Button Clicked ", Toast.LENGTH_SHORT).show();
+            default:
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
