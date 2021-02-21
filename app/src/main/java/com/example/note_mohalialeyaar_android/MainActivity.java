209bd3ffb91e1d9fe1d48c129d77631ec921b906
Amarvir Singh <amarvirgill06@gmail.com>
2021-02-21 00:50:45 -0500

    addActivty added

--- app/src/main/java/com/example/note_mohalialeyaar_android/MainActivity.java
+++ app/src/main/java/com/example/note_mohalialeyaar_android/MainActivity.java
@@ -2,18 +2,40 @@ package com.example.note_mohalialeyaar_android;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton addBtn;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv);
+        addBtn = findViewById(R.id.addBtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent addNoteIntent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivity(addNoteIntent);
+            }
+        });
+
+
     }
 
 
